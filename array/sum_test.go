e3515be7c49d6b99553f372f46bb4831f525e4ef
Roman Kubu <kuburoman@seznam.cz>
2018-11-24 00:03:53 +0100

    Added function for sum of slices

--- array/sum_test.go
+++ array/sum_test.go
@@ -1,6 +1,7 @@
 package array
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -38,4 +39,37 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2, 3}, []int{0, 5})
+	want := []int{6, 5}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d want %d", got, want)
+	}
+
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
 }
