98463a5b9e8509baee9a92ffe846588e17ce0c93
Roman Kubu <kuburoman@seznam.cz>
2018-11-23 23:14:15 +0100

    Added function for sum on slices

--- array/sum.go
+++ array/sum.go
@@ -1,10 +1,10 @@
 package array
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
-	for index := 0; index < 5; index++ {
-		sum = sum + numbers[index]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
--- array/sum_test.go
+++ array/sum_test.go
@@ -6,13 +6,36 @@ import (
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	assertCorrectMessage := func(t *testing.T, got, want int, given []int) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%d' want '%d' given, %v", got, want, given)
+		}
 	}
 
+	t.Run("saying hello to people", func(t *testing.T) {
+
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		assertCorrectMessage(t, got, want, numbers)
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("saying hello to people", func(t *testing.T) {
+
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		assertCorrectMessage(t, got, want, numbers)
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
